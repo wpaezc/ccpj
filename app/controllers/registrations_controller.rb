385ed6da18b8dd9376a8d38f073746593354ddd6
Wenceslao Paez <wcpaez@gmail.com>
2022-04-28 12:55:21 -0500

    Show qr

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,5 +4,17 @@ class RegistrationsController < ApplicationController
 
   def show
     @registration = Registration.find_by(uuid: params[:id])
+    if @registration
+      qr = RQRCode::QRCode.new(request.url)
+      @svg = qr.as_svg(
+        color: "000",
+        shape_rendering: "crispEdges",
+        module_size: 4,
+        standalone: true,
+        use_path: true
+      )
+    else
+      redirect_to root_path
+    end
   end
 end
